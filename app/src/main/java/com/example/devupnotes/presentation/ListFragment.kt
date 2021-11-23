2aa7b5e26942cb96a776e2e21011593320b0850d
Chris Athanas <chris@crowdskout.com>
2021-11-22 23:24:12 -0600

    Add view binding, update nav library version

--- app/src/main/java/com/example/devupnotes/presentation/ListFragment.kt
+++ app/src/main/java/com/example/devupnotes/presentation/ListFragment.kt
@@ -1,11 +1,12 @@
 package com.example.devupnotes.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.devupnotes.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.devupnotes.databinding.FragmentListBinding
 /**
  * A simple [Fragment] subclass.
  * Use the [ListFragment.newInstance] factory method to
@@ -13,19 +14,26 @@ import com.example.devupnotes.R
  */
 class ListFragment : Fragment() {
 
+    private lateinit var bind: FragmentListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+
+        bind = FragmentListBinding.inflate(inflater, container, false);
+        return bind.root;
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        bind.fabAddNote.setOnClickListener{ goToNoteDetails() }
     }
 
-    private fun goToNoteDetails(id: Long = 0) {
-        val action = ListFragmentDirections.actionGoToNote()
-//        Navigation.
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action = ListFragmentDirections.actionGoToNote(id)
+        view?.let { Navigation.findNavController(it).navigate(action) }
+//        Navigation.findNavController(bind.notesListView).navigate(action) // alternate way
     }
 }
--- app/src/main/java/com/example/devupnotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/devupnotes/presentation/NoteFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.devupnotes.R
+import com.example.devupnotes.databinding.FragmentListBinding
+import com.example.devupnotes.databinding.FragmentNoteBinding
 
 
 /**
@@ -14,11 +17,22 @@ import com.example.devupnotes.R
  * create an instance of this fragment.
  */
 class NoteFragment : Fragment() {
+
+    private lateinit var bind: FragmentNoteBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+//        return inflater.inflate(R.layout.fragment_note, container, false)
+
+        bind = FragmentNoteBinding.inflate(inflater, container, false);
+        return bind.root;
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bind.fabSaveButton.setOnClickListener{ Navigation.findNavController(it).popBackStack() }
+    }
 }
